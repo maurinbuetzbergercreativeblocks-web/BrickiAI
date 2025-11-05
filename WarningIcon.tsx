4aa1ff89f9f9724f74c864991393ad23325176fe
maurinbuetzbergercreativeblocks-web <maurinbuetzberger.creativeblocks@outlook.com>
2025-11-05 17:44:48 +0100

    Add files via upload

---
+++ WarningIcon.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+export const WarningIcon: React.FC = () => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    className="h-5 w-5 flex-shrink-0" 
+    viewBox="0 0 20 20" 
+    fill="currentColor">
+    <path 
+        fillRule="evenodd" 
+        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.22 3.006-1.742 3.006H4.42c-1.522 0-2.492-1.672-1.742-3.006l5.58-9.92zM10 13a1 1 0 110-2 1 1 0 010 2zm-1-4a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+        clipRule="evenodd" 
+    />
+  </svg>
+);
